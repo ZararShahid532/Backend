c217eaadb6c97399be757d3162261ea784e29ebf
Zarar Shahid <zararshahid854@gmail.com>
2025-01-08 14:20:20 +0500

    Utils Code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGN,// manage ap ka Url kha sa ha rha ha
+    credentials: true 
+})) // Used in middleware or configration
+
+app.use(express.json({
+    limit: "16kb"
+}))
+app.use(express.urlencoded({extended: true,limit: "16kb"})) // URL data handle
+
+app.use(express.static("public"))  //sometime store file folder create public assit
+
+app.use(cookieParser()); // Server sa user ka browser ki cookies access and set kar skta ha 
+export{app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asycHandler = (requestHandler) => {
+       (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+       }
+}
+
+export {asycHandler}
